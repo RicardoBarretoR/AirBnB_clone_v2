bc237718f7d7e326a62fee9c15f9984ee9e7070e
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-08-17 23:56:07 -0500

    fixing task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #set up web server for the deployment web_static
 sudo apt-get update
-sudo apt-get install -y nginx
+sudo apt-get -y install nginx
 sudo mkdir /data/
 sudo mkdir /data/web_static/
 sudo mkdir /data/web_static/releases/
@@ -9,13 +9,13 @@ sudo mkdir /data/web_static/shared/
 sudo mkdir /data/web_static/releases/test/
 echo "<!DOCTYPE html>
 <html>
- <head>
- </head>
- <body>
-  Holberton School
- </body>
- </html>" | sudo tee /data/web_static/releases/test/index.html
+	<head>
+	</head>
+	<body>
+		 Holberton School
+	</body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
  sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
  sudo chown -R ubuntu:ubuntu /data/
- sudo sed -i '39 i\ \tlocation /hbtn_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
+ sudo sed -i '/pass  the PHP/i \\tlocation /hbtn_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
  sudo service nginx restart
