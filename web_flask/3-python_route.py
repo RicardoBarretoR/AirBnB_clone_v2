29cb15bb6d70074383461c418ff352b689323245
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-09-03 05:37:05 +0000

    adding task 3:showing the default value of text

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""start a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Function showing Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Function showing HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def DisplayC(text):
+    """Function showing C and the text input"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def DisplayPython(text='is cool'):
+    """Function showing the default value of text"""
+    return "python {}".format(text.replace('_', ' '))
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
