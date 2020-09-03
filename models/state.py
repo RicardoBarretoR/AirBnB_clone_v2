0a50912e031537b75707d55be1a0d79b86c94bfa
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-09-02 22:17:56 -0500

    adding update of models folder

--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import os
+import models
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    # name = ""
+    __tablename__ = 'states'
+    if os.environ.get("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="all,delete")
+
+    @property
+    def cities(self):
+        """get all cities
+        """
+        cities = models.storage.all(City)
+        city_l = []
+        for i in cities.values():
+            if self.id == i.state_id:
+                city_l.append(i)
+        return city_l
